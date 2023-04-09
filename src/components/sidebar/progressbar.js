266220c9428e2e90efed99cd040b67694a2a4556
Amelia Jochna <amei70bieber@gmail.com>
2023-04-09 18:53:17 +0200

    sidebar update

---
+++ src/components/sidebar/progressbar.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import "./style.css";
+import blueVector from "./images/bluedot.png";
+class ProgressBar extends Component {
+  percentage = () => {
+    // const doneTasks = [...this.props.tasks].filter((t) => t.active === false);
+    // const ratio = (doneTasks.length / this.props.tasks.length) * 100;
+    const ratio = 90;
+    return ratio + "%";
+  };
+
+  topposition = () => {
+    const height = parseInt(this.percentage(), 10);
+    const topvalue = 640 - height * 4;
+    return topvalue + "px";
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="progress" style={{ height: "25px" }}>
+          <div className="progress-bar" style={{ width: this.percentage() }} />
+        </div>
+        <img
+          className="percent-dot"
+          src={blueVector}
+          alt=""
+          style={{ top: this.topposition() }}
+        />
+        <div className="percent-text" style={{ top: this.topposition() }}>
+          {this.percentage()}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ProgressBar;
--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
 import "./style.css";
-import bluevector from "./images/bluedot.png";
-import pinkvector from "./images/pinkdot.png";
+import BlueVector from "./images/bluedot.png";
+import PinkVector from "./images/pinkdot.png";
+import ProgressBar from "./progressbar";
+import LogOut from "./images/logout.png";
+import ProfilePicture from "./images/profilepicture.png";
 
 class Sidebar extends Component {
   state = {};
@@ -9,13 +12,28 @@ class Sidebar extends Component {
     return (
       <div>
         <div className="logo-corner">
-          <img className="pink-vector" src={pinkvector} alt="" />
-          <img className="blue-vector" src={bluevector} alt="" />
-          <div className="brand-name">Some logo</div>
+          <img className="pink-vector" src={PinkVector} alt="" />
+          <img className="blue-vector" src={BlueVector} alt="" />
+          <div className="brand-name">Productivity</div>
         </div>
         <div className="side-bar">
           <div className="my-progress">My progress</div>
+          <button
+            className="transparent-button"
+            onClick={() => console.log("WYLOGUJ")}
+          >
+            <img className="log-out-icon" src={LogOut} alt="" />
+            <div className="log-out-text">Log Out</div>
+          </button>
+          <button
+            className="transparent-button"
+            onClick={() => console.log("MOJ PROFIL")}
+          >
+            <img className="profile-pink-dot" src={PinkVector} />
+            <img className="my-profile-icon" src={ProfilePicture} />
+          </button>
         </div>
+        <ProgressBar />
       </div>
     );
   }
